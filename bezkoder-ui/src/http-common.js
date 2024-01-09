5a6d7971688f82a595ccc6109031bc43b4a61317
Jake Street <streetjake@gmail.com>
2024-01-09 13:31:57 -0500

    changes for nginx ingress

--- bezkoder-ui/src/http-common.js
+++ bezkoder-ui/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: window.location.origin + '/api',
+  baseURL: window.location.origin + '/bezkoder-ui/api',
   headers: {
     "Content-type": "application/json"
   }
